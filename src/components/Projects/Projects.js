17a5428cca0be689850f035bb71db12c50dfdda4
Cong Cao <caocong2404@gmail.com>
2025-03-13 00:46:25 +0700

    Update projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import vsat from "../../Assets/Projects/vsat.png";
 
 function Projects() {
   return (
@@ -23,68 +18,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={vsat}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={vsat}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={vsat}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={vsat}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={vsat}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={vsat}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="VSAT"
+              description="VSAT is an SAT preparation platform that personalizes study plans, tracks student progress in real-time, and adapts test strategies based on performance. Designed for students, teachers, and administrators, it streamlines learning management, test preparation, and performance evaluation. By leveraging technology, VSAT ensures a more efficient, adaptive, and results-driven approach to SAT success."
+              ghLink="https://github.com/caocong2KB6/vsat-server"
+              demoLink="https://github.com/caocong2KB6/vsat-server"
             />
           </Col>
         </Row>
